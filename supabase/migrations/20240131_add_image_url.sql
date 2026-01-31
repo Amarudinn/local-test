feb1d480c6d38189b70c42b73beed0a0063bd409
Amarudinn <masamar707@gmail.com>
2026-01-31 21:55:03 +0700

    Cleanup: Stop tracking supabase folder as requested

--- supabase/migrations/20240131_add_image_url.sql
+++
@@ -1,5 +0,0 @@
--- Add image_url column to debates table
-ALTER TABLE debates ADD COLUMN IF NOT EXISTS image_url TEXT;
-
--- Comment on column
-COMMENT ON COLUMN debates.image_url IS 'Optional URL for debate cover image (IPFS via Pinata)';
